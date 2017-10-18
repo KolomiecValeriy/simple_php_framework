a6c845f8434e2ddd8f86ac7911f9578baa59655b
Valeriy Kolomiets <vel202007@gmail.com>
2017-10-19 00:26:48 +0300

    Get controller from route with HttpKernel

--- src/app.php
+++ src/app.php
@@ -14,13 +14,19 @@ function is_leap_year($year = null) {
 $routes = new Routing\RouteCollection();
 $routes->add('leap_year', new Routing\Route('/is_leap_year/{year}', array(
     'year' => null,
-    '_controller' => function ($request) {
-        if (is_leap_year($request->attributes->get('year'))) {
+    '_controller' => 'LeapYearController::indexAction',
+)));
+
+class LeapYearController
+{
+    public function indexAction($year)
+    {
+        if (is_leap_year($year)) {
             return new Response('Yep, this is a leap year!');
         }
 
         return new Response('Nope, this is not a leap year.');
     }
-)));
+}
 
 return $routes;
